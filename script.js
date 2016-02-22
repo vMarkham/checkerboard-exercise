0476f5ddbce0e39d0f2e938afcad6683239cafa5
Vincent Markham <vincent.markham@gmail.com>
2016-02-21 23:21:16 -0800

    Complete random color

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 for(i = 0; i < 81; i++){
   var newD = document.createElement('div');
   var beachBod = document.getElementsByTagName('body')[0];
+  var randCol = 'rgb(' + (Math.floor(Math.random()*256)) + ", " + (Math.floor(Math.random()*256)) + ", " + (Math.floor(Math.random()*256)) + ")";
   newD.style.width = "11.1%";
   newD.style.paddingBottom = "11.1%";
   newD.style.float = "left";
-  if (i % 2 === 0) {
-    newD.style.backgroundColor = "red";
-  } else {
-    newD.style.backgroundColor = "black";
-  }
+
+  newD.style.backgroundColor = randCol;
   beachBod.appendChild(newD);
 }
