4a8809c9a2dd6fd6096f6f3814d06bc262bc4ba4
Vincent Markham <vincent.markham@gmail.com>
2016-02-21 23:07:50 -0800

    Complete black-red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Your JS goes here
+for(i = 0; i < 81; i++){
+  var newD = document.createElement('div');
+  var beachBod = document.getElementsByTagName('body')[0];
+  newD.style.width = "11.1%";
+  newD.style.paddingBottom = "11.1%";
+  newD.style.float = "left";
+  if (i % 2 === 0) {
+    newD.style.backgroundColor = "red";
+  } else {
+    newD.style.backgroundColor = "black";
+  }
+  beachBod.appendChild(newD);
+}
