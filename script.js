1332b0a5e6511104472385342bc334bd0e581ec6
Vincent Markham <vincent.markham@gmail.com>
2016-02-29 22:46:40 -0800

    Lint and fix

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-//This is incomplete.
-
-
 (function() {
   'use strict';
-
   for (var i = 0; i < 81; i++) {
     var newD = document.createElement('div');
     var beachBod = document.getElementsByTagName('body')[0];
-    // var randCol = 'rgb(' + Math.floor(Math.random() * 256) +
-    //   ', ' + Math.floor(Math.random() * 256) + ', ' +
-    //   Math.floor(Math.random() * 256) + ')';
-    var thisColor = 'rgb(' + (25 + i * 2) + ', ' + (150 - i) + ', ' + (0 + i * 2) + ')';
+    var thisColor = 'rgb(' + (25 + i * 2) + ', ' +
+    (150 - i) + ', ' + (0 + i * 2) + ')';
 
     newD.style.width = '11.1%';
     newD.style.paddingBottom = '11.1%';
     newD.style.float = 'left';
-
     newD.style.backgroundColor = thisColor;
     beachBod.appendChild(newD);
   }
